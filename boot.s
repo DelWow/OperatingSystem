ae06d8e86f873a8e0e72d26e85bbcb6d705aeec4
Annas Amare <annasamar28@gmail.com>
2026-01-16 22:12:37 -0500

    feature: Stack pointer + multiboot loader

--- boot.s
+++ boot.s
@@ -1,3 +1,5 @@
+
+# MULTIBOOT HEADER
 /* Declare our constants for a multiboot header */
 .set ALIGN,  1<<0
 
@@ -12,4 +14,33 @@
 /*Starting off from ALIGN and MEMINFO, these are 2 bitwise operators that allow for multiple bool flags to be encoded into a
 single integer value. We can then use this encoded value to make sure that things are stored in memory in values of 4096.
 In 86x a page is defined as 4KB or 4096 bytes. If a value is a multiple of 4096, we can call that page alligned. 
+*/
+
+.section .multiboot /* Partitions the following data into a section called multiboot */
+
+.align 16 
+
+.long MLB
+
+.long FLAGS
+
+.long CHECKSUM
+
+/*MLB + FLAGS + CHECKSUM -> make up the core of the multiboot header. So we are essentially creating an object called multiboot which must of 
+16 byte aligned (multiple of 16) and writes 3 32-bit numbers MLB, FLAGS and CHECKSUM which GRUB will scan for*/
+/*GRUB only scans the first 8 KiB (8192 bytes) of a file*/
+
+.section .bss
+.align 16
+stack_bottom
+.skip_bottom 16384 #16 KiB
+stack_top:
+
+/*Since our multiboot standard does not intialize an SP (stack pointer), we must initialize it manually*/
+/* Our stack pointer:
+    - Downward initialized
+    - Size of 16384 bytes
+    - 16 bits or 4 byte alignment
+    - Stack is in its own section, it is marked as NOBITS (zero-initialized memory)
+
 */
